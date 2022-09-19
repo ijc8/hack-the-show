d14400ab5d3f5842f486fae0c2bde66df1df132d
Ian Clester <ijc@ijc8.me>
2022-09-19 12:50:21 -0400

    Extend shared state and UI to support 8 parameters

--- main.py
+++ main.py
@@ -1,38 +1,48 @@
 import asyncio
+import json
+
 from sanic import Sanic
 
 app = Sanic("WebsocketExample")
 
-counter = 0
+# TODO: Replace with actual parameters.
+params = list("ABCDEFGH")
+state = {p: 0 for p in params}
 update = asyncio.Event()
 
 @app.websocket("/ws")
 async def websocket(request, ws):
     # A coroutine is spawned for each connected client.
-    global counter
+    global state
+    client_state = state.copy()
     print("New websocket connection from", request.ip)
-    # New connection: send the current value of the counter.
-    await ws.send(str(counter))
+    # New connection: send the current state.
+    await ws.send(json.dumps(state))
     recv = asyncio.create_task(ws.recv())
     updated = asyncio.create_task(update.wait())
     while True:
-        # Wait for a new message from our client, or an update to the counter (from another client).
+        # Wait for a new message from our client, or an update to the state (from another client).
         # Handle whichever happens first.
         done, _ = await asyncio.wait({recv, updated}, return_when=asyncio.FIRST_COMPLETED)
         for task in done:
             if task is recv:
                 # Uncomment to see what we got from the client:
-                # message = task.result()
+                message = json.loads(task.result())
                 # print(f"Got message from {request.ip}: {message}")
-                # Got a message from the client; increment the counter.
-                counter += 1
+                # Got a message from the client; update the state.
+                param = message["param"]
+                delta = message["delta"]
+                state[param] += delta
                 # Signal to all coroutines that they should send updates to their clients.
                 update.set()
                 update.clear()
                 recv = asyncio.create_task(ws.recv())
             elif task is updated:
-                # Counter updated: send the new value to our client.
-                await ws.send(str(counter))
+                # State updated.
+                # Compute the diff with the client's local state, and send the necessary updates.
+                diff = {k: v for k, v in state.items() if v != client_state[k]}
+                client_state.update(state)
+                await ws.send(json.dumps(diff))
                 updated = asyncio.create_task(update.wait())
 
 app.static("/", "index.html")
